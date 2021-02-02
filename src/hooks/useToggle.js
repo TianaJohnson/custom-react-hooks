e2e1fe0f60c7dac9c631baaa6117c1832ff7d7d5
Tiana <Tiana.marie013@gmail.com>
2021-02-02 07:48:11 -0700

    array to object

--- src/hooks/useToggle.js
+++ src/hooks/useToggle.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 export const useToggle = (initial) => {
     const [isToggled, setToggle] = useState(initial);
     const toggle = () => setToggle(prevState => !prevState);
-    return [isToggled, setToggle, toggle];
+    // return [isToggled, setToggle, toggle];
+    return {isToggled, setToggle, toggle};
 
 };
