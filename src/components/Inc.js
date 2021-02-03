a9f9123d83eeed901d40e495c39963ecea8cf9a1
Tiana <Tiana.marie013@gmail.com>
2021-02-03 09:36:27 -0700

    inc componenet

---
+++ src/components/Inc.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Inc = () => {
+    return (
+        <div>
+            <button></button>
+            <button></button>
+        </div>
+    )
+}
+
+export default Inc;
