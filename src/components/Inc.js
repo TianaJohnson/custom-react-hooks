a50d7296e44583f1e55e1cecff54ebbe2ef6ee8a
Tiana <Tiana.marie013@gmail.com>
2021-02-04 18:30:02 -0700

    custom hook

--- src/components/Inc.js
+++ src/components/Inc.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { useInc } from '../hooks/useInc';
 
 export const Inc = () => {
 
+    const [value, { dec, inc}] = useInc();
+
     return (
         <div>
-            <button onClick={}>-</button>
+            <button onClick={dec}>-</button>
             {value}
-            <button onClick={}>+</button>
+            <button onClick={inc}>+</button>
         </div>
     )
 }
--- src/hooks/useInc.js
+++ src/hooks/useInc.js
@@ -4,11 +4,11 @@ export const useInc = (initial = 0) => {
     const [value, setValue] = useState(initial);
 
     const inc = () => {
-        () => setValue(prevState => prevState +1)
+       setValue(prevState => prevState +1)
     }
 
     const dec = () => {
-        { () => setValue(prevState => prevState -1)}
+       setValue(prevState => prevState -1)
     }
      return  [ value, {inc, dec} ]
 }
