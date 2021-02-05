3bfca2f0bd05ae1de78c465ed4b87e5e4c92e51e
Tiana <Tiana.marie013@gmail.com>
2021-02-04 19:14:32 -0700

    nums replavced by varialbles

--- src/hooks/useInc.js
+++ src/hooks/useInc.js
@@ -3,16 +3,17 @@ import { useState } from 'react';
 export const useInc = ({
     maxValue = 100000,
     minValue = -100000,
-    initial = 0
+    initial = 0,
+    step = 1
 }) => {
     const [value, setValue] = useState(initial);
 
     const inc = () => {
-       setValue(prevState => (prevState >= maxValue ? maxValue : prevState + 1));
+       setValue(prevState => (prevState >= maxValue ? maxValue : prevState + step));
     };
 
     const dec = () => {
-       setValue(prevState =>(prevState >= minValue ? minValue : prevState - 1));
+       setValue(prevState =>(prevState >= minValue ? minValue : prevState - step));
     };
 
     const reset = () => setValue(initial);
