9fae9e428b09b22ad4b195c487df9b2acaab5765
Tiana <Tiana.marie013@gmail.com>
2021-02-07 15:06:53 -0700

    color change on hover

--- src/components/Hover.js
+++ src/components/Hover.js
@@ -1,23 +1,18 @@
-import React, { useState, useMemo } from 'react'
+import React from 'react'
+import {useHover} from '../hooks/useHover';
 import { Card } from '../Elements';
 import black from '../black.png';
 
 
+
 const Hover = () => {
 
-    const [isHovered, setIsHovered] = useState(false);
-    console.log("is hovered", isHovered);
-
-    const bind = useMemo(() => {
-        return {
-            onMouseOver: () => setIsHovered(true),
-            onMouseLeave: () => setIsHovered(false),
-        }
-    }, []);
+    const [isHovered, bind] = useHover();
+    console.log("is hovered", isHovered)
 
     return (
         <div>
-            <Card {...bind} style={{ background: "var(--black)" }}>
+            <Card {...bind} style={{ background: isHovered ? "var(--black)" : "var(--purp)"}}>
             <h3>Some card</h3>
             <img src={black} />
           </Card>
