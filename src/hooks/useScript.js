729d238ce5991856ecd8b1dceb1ab0aa4ce74616
Tiana <Tiana.marie013@gmail.com>
2021-02-10 18:46:25 -0700

    outline

---
+++ src/hooks/useScript.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+
+let cached = [];
+
+const useScript = (src) => {
+
+    const [ status, setStatus ] = useState({
+
+        loaded: false,
+        error: false
+    })
+
+    useEffect (() => {
+        effect
+
+        return () => {
+            cleanup
+        }
+    })
+
+
+
+}
