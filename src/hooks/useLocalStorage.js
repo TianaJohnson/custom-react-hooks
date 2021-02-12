69338884ee4c0c9e5abc66892ce586e5dcb119ae
Tiana <Tiana.marie013@gmail.com>
2021-02-12 14:59:21 -0700

    works

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,12 +1,12 @@
 import { useState, useEffect} from 'react';
 
 export const useLocalStorage = ( key, initial ) => {
-    const [value, setValue ]= useState(initial);
+    const item = window.localStorage.getItem(key);
+    const [value, setValue ]= useState(item || initial);
 
     useEffect(() => {
-        
-        window.localStorage.setItem(key, value)
-    }, [value, key])
+           window.localStorage.setItem(key, value)
+    }, [value, key, initial])
 
     return [ value, setValue];
 };
